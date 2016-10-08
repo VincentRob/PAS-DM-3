1b00871ab433e6a7e27f5a2f77059fecac8f3bb1
pachinus <leonard.panichi@gmail.com>
2016-10-08 18:52:43 +0200

    Update Exercice 1.py

--- Exercice 1.py
+++ Exercice 1.py
@@ -4,14 +4,3 @@ Created on Sat Oct  8 17:51:24 2016
 
 @author: Leonard and Marvin
 """
-
-print("Small change again and again and again")
-print("Hi Pachinus !")
-
-
-print("Test of pull request")
-
-
-print("Ceci est un test de modif de changement avant de fusionner avec la branche master")
-
-print("Je surenchérie qui dit mieux !?")
